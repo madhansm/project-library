79bd464f79eb93e945b90616c4116c0d9eec5695
Madhan Samiappan <madhan.samiappan@hotmail.com>
2021-11-19 14:06:57 +0530

    added read button and delete button

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const anarchy = {
     genre: 'History',
     pages: 576,
     description: "\'Dalrymple is a superb historian with a visceral understanding of India . A book of beauty\' - Gerard DeGroot",
-    completion: false
+    completion: true
 }
 
 let cardsContainer = document.querySelector('.cardsContainer');
@@ -31,11 +31,33 @@ function createBook(name, author, language, genre, pages, description, completio
     this.completion = completion;
 }
 
+
+
 function displayBook(book, card) {
     console.log(book.name);
     
     const h2 = document.createElement('h2');
     h2.append(book.name);
+
+    const bookRead = document.createElement('div');
+    bookRead.classList.add('read');
+    const readText = document.createTextNode('Read');
+    bookRead.append(readText);
+    
+    const buttonReadYes = document.createElement('button');
+    buttonReadYes.classList.add(`readYes`);
+    const buttonTextYes = document.createTextNode('\u2714');
+    buttonReadYes.append(buttonTextYes);
+    bookRead.appendChild(buttonReadYes);
+
+    const buttonReadNo = document.createElement('button');
+    buttonReadNo.classList.add(`readNo`);
+    const buttonTextNo = document.createTextNode("\u2717");
+    buttonReadNo.append(buttonTextNo);
+    bookRead.appendChild(buttonReadNo);
+
+
+    h2.appendChild(bookRead);
     card.appendChild(h2);
 
     for (const key in book){
@@ -57,9 +79,46 @@ function displayBook(book, card) {
 
     }
 
+    const buttonDeleteBook = document.createElement('button');
+    buttonDeleteBook.classList.add('deleteBook');
+    const buttonTextDeleteBook = document.createTextNode("Delete Book");
+    buttonDeleteBook.append(buttonTextDeleteBook);
+    card.appendChild(buttonDeleteBook);
 
-    
+    if (book.completion) {
+        buttonReadNo.style.display = "none";
+    } else {
+        buttonReadYes.style.display = "none";
+    }
+
+    buttonReadYes.addEventListener('click', () => {
+        buttonReadYes.style.display = "none";
+        buttonReadYes.parentElement.querySelector('.readNo').style.display = "inline";
+        book.completion = false;
+
+    });
+
+    buttonReadNo.addEventListener('click', () => {
+        buttonReadNo.style.display = "none";
+        buttonReadNo.parentElement.querySelector('.readYes').style.display = "inline";
+        book.completion = true;
+
+    });
+
+    buttonDeleteBook.addEventListener('click', () => card.remove());
+
 }
+
+function changeToNo() {
+    this.style.display = "none";
+    this.parentElement.querySelector('.readNo').style.display = "inline";
+}
+
+function changeToYes() {
+    this.style.display = "none";
+    this.parentElement.querySelector('.readYes').style.display = "inline";
+}
+
 
 myLibrary.forEach(book => {
     const card = document.createElement('div');
@@ -74,3 +133,6 @@ const popUp = document.getElementById('popUp');
 
 
 addBook.addEventListener('click', () => popUp.style.display="block");
+
+let readYesButtons = document.querySelectorAll('.readYes');
+let readNoButtons = document.querySelectorAll('.readNo');
